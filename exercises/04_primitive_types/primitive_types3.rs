87c6c4fef4176b204505cfc995116c615444ecc5
echavarriasam <echavarriasam@gmail.com>
2024-11-30 17:56:55 +0000

    arrays

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,8 +1,8 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
-    // let a = ???
+    let a: [i32; 1] = [0];
 
-    if a.len() >= 100 {
+    if !a.is_empty() {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
