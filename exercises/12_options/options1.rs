207ed9f961514e406c642551a14e7331b83ea9a3
echavarriasam <echavarriasam@gmail.com>
2024-12-02 06:02:44 +0000

    opt1

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -1,13 +1,28 @@
 // This function returns how much icecream there is left in the fridge.
-// If it's before 22:00 (24-hour system), then 5 scoops are left. At 22:00,
-// someone eats it all, so no icecream is left (value 0). Return `None` if
-// `hour_of_day` is higher than 23.
+// If it's before 22:00 (24-hour system), then 5 scoops are left.
+// At 22:00, someone eats it all, so no icecream is left (value 0).
+// Return `None` if `hour_of_day` is higher than 23.
 fn maybe_icecream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
+    // match hour_of_day {
+    //     0..=21 => Some(5),
+    //     22..=23 => Some(0),
+    //     _ => None,
+    // }
+    if hour_of_day < 22 {
+        Some(5)
+    } else if (22..=23).contains(&hour_of_day) {
+        Some(0)
+    } else {
+        None
+    }
 }
 
 fn main() {
     // You can optionally experiment here.
+    let icecreams = maybe_icecream(203).unwrap_or_default();
+    // let icecreams = maybe_icecream(22).unwrap();
+    println!("icecreams: {}", icecreams);
 }
 
 #[cfg(test)]
@@ -18,7 +33,7 @@ mod tests {
     fn raw_value() {
         // TODO: Fix this test. How do you get the value contained in the
         // Option?
-        let icecreams = maybe_icecream(12);
+        let icecreams = maybe_icecream(12).unwrap_or(0);
 
         assert_eq!(icecreams, 5); // Don't change this line.
     }
