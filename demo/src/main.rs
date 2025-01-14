b3e07d1cdd8480b58198e5da863b1c9831fdd08d
Angel Echavarria <echavarriasam@gmail.com>
2025-01-14 03:24:01 +0000

    single linked list

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,3 +1,27 @@
+struct Node {
+    value: String,
+    next: Option<Box<Node>>,
+}
+
+impl Node {
+    fn new(value: String) -> Node {
+        Node { value, next: None }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut head = Node::new("Hello".to_string());
+    let mut tail = &mut head;
+    for i in 0..10 {
+        let content = format!("Hello {}", i + 1);
+        let node = Node::new(content);
+        tail.next = Some(Box::new(node));
+        tail = tail.next.as_mut().unwrap();
+    }
+
+    let mut current = &head;
+    while let Some(node) = current.next.as_ref() {
+        println!("{}", node.value);
+        current = node;
+    }
 }
